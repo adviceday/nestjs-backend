4978ad3b2fe3219e903564776b8579750363b362
Gregor Tokarev <zippper123915@gmail.com>
2022-02-26 08:02:19 +0300

    Sll to production mode

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ rsa_key_size=4096
 docker_compose_config_path=./docker-compose.production.yml
 data_path="./certbot"
 email="genag1995@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
