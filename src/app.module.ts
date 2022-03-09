dd244fd25d8a9fbf59a0780587178d372edf8c05
Gregor Tokarev <zippper123915@gmail.com>
2022-03-09 16:49:16 +0300

    Remove admin-bro

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormFactory } from './config/typeorm.factory';
-import { AdminModule } from '@adminjs/nestjs';
-import { adminjsFactory } from './config/adminjs.factory';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { SettingsModule } from './settings/settings.module';
@@ -25,11 +23,6 @@ import { ScheduleModule } from '@nestjs/schedule';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: typeormFactory,
-    }),
-    AdminModule.createAdminAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: adminjsFactory,
     }),
     ScheduleModule.forRoot(),
     AuthModule,
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Database, Resource } from '@adminjs/typeorm';
-import AdminJS from 'adminjs';
 import { ValidationPipe } from '@nestjs/common';
 
 /**
  * ignore
  */
 async function bootstrap() {
-  AdminJS.registerAdapter({ Database, Resource });
   const app = await NestFactory.create(AppModule);
 
   // set globals
